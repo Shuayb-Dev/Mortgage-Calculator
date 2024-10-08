d25f0b3d6be8e10dc2d6d373f25d0ce1456c88ff
Shuayb-Dev <shuayb.abdulkadir5@gmail.com>
2024-10-08 18:29:31 +0100

    Started work on calulation logic

---
+++ script.js
@@ -0,0 +1,31 @@
+function calculate() {
+  //Shows the result
+  let showresult = document.getElementById("calculated-result");
+
+  //Takes User Inputs
+  let userInput = document.getElementById("mortgage-amount").value;
+  let terms = document.getElementById("terms").value;
+  let rate = document.getElementById("rate").value;
+
+  document.getElementById("result").style.display = "none";
+  showresult.style.display = "block";
+  showresult.innerHTML = userInput;
+}
+
+function checkMortgageType() {
+  let selectedTypeText = document.getElementById("selectedTypeText");
+
+  const selectedType = document.querySelector(
+    'input[name="mortgage-type"]:checked'
+  );
+
+  if (selectedType.value === "intrest-only") {
+    selectedTypeText.style.display = "block";
+    selectedTypeText.innerHTML = "Interest Only";
+  } else if (selectedType.value === "repayment") {
+    selectedTypeText.style.display = "block";
+    selectedTypeText.innerHTML = "Repayment";
+  } else {
+    selectedTypeText.style.innerHTML = "Please select a type";
+  }
+}
